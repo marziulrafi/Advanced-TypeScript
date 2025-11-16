4fed2617cd69969c218df716ef3ef0ff37fc0364
K M Marziul Karim <marziulrafi@gmail.com>
2025-11-16 13:00:58 +0600

    Generics

--- dist/typeAssertion.js
+++ dist/typeAssertion.js
@@ -1,20 +1,23 @@
-var anything;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let anything;
 anything = "Marziul";
-var kgToGMConverter = function (input) {
+const kgToGMConverter = (input) => {
     if (typeof input === "number") {
         return input * 1000;
     }
     else if (typeof input === "string") {
-        var value = input.split(" ")[0];
-        return "Converted output is: ".concat(Number(value) * 1000);
+        const [value] = input.split(" ");
+        return `Converted output is : ${Number(value) * 1000}`;
     }
 };
-var result1 = kgToGMConverter(2);
-console.log({ result1: result1 });
-var result2 = kgToGMConverter("2 kg");
-console.log({ result2: result2 });
+const result1 = kgToGMConverter(2);
+console.log({ result1 });
+const result2 = kgToGMConverter("2 kg");
+console.log({ result2 });
 try {
 }
 catch (err) {
     console.log(err.meesage);
 }
+//# sourceMappingURL=typeAssertion.js.map
